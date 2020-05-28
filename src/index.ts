bb6fe0d2351be2861d84cb397aaa1e43dcdc7aa8
Chris Bode <chris@eonz.net>
2020-05-28 19:05:09 -0400

    command line option: --token
    Added command line options that allow api tokens to be specified apart from config.json. This is mainly to allow me to test without scrubbing out my api token every time I commit.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,31 @@ import { Personality } from "./personality";
 import { match } from "assert";
 import { Database } from "sqlite";
 
-const configSource  =   fs.readFileSync("./config.json", { encoding: "utf8" });
+import yargs from "yargs";
+
+const argv = yargs
+    .help("h").alias("h", "help")
+    .option("config", {
+        alias: "c",
+        default: "./config.json",
+        normalize: true,
+        description: "specifies an alternative config.json file"
+    })
+    .option("token", {
+        alias: "t",
+        type: "string",
+        description: "specifes an external file containing the application token to use for the bot"
+    }).argv;
+
+const configSource  =   fs.readFileSync(argv.config, { encoding: "utf8" });
 const config        =   JSON.parse(configSource);
+
+if (argv.token) {
+    const data = fs.readFileSync(argv.token, { encoding: "utf8" });
+    config.token = data.trim();
+    console.log("using token file");
+}
+
 let bot             =   new GatekeeperBot(config);
 
 bot.connect();
